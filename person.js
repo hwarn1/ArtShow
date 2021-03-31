251a1d73dc5abcec6b338093c1aa5f9120b0aab3
hwarn1 <79218512+hwarn1@users.noreply.github.com>
2021-03-31 16:24:25 -0400

    Update person.js
    This is the correct person file.

--- person.js
+++ person.js
@@ -1,39 +1,54 @@
-class Animation {
-  constructor(x,y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-    this.count=0;
-    this.xoff=10;
+// Daniel Shiffman
+// https://www.kadenze.com/courses/the-nature-of-code
+// http://natureofcode.com/
+// Session 2: Array of Particles, multiple forces
+
+function Person(img) {
+  this.pos = createVector(10, height - 300);
+  this.vel = createVector(2, 0);
+  this.acc = createVector(0, 0);
+  this.pic = img;
+  this.score = 0;
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
   }
-  update() {
-    this.pos.add(this.vel);
+
+  this.update = function() {
     this.vel.add(this.acc);
+    this.pos.add(this.vel);
     this.acc.set(0, 0);
-    this.count++;
-    this.xoff +=random(0.01);
+  }
+
+  this.display = function() {
+    fill(200, 70,70);
+    stroke(255);
+    textSize(30);
+    text("Papers collected: " + this.score, this.pos.x, 100);
+    image(this.pic, this.pos.x, this.pos.y, 50, 40);
+    
+      //countdown
+    fill(255);
+    textSize(20);
+    text("Distance remaining: " + (3000 - this.pos.x + "m"), this.pos.x + 100, 399);
+  }
+
+  this.edges = function() {
+  if (this.pos.y <= 0) {
+      this.vel.y = 0;
+      this.pos.y = 0;
+    }
 
   }
+
+ 
   
-  applyForce(f){
-    //this.acc.add(f);
-  }
-  display() {
-    //background(0)
-    push();
-    //The only 3 transformations you need
-    translate(windowWidth/2, windowHeight/2)
-    scale(noise(this.xoff)%0.2)
-    rotate(noise(this.xoff)/0.1)
-
-    fill((this.pos.x *this.pos.x) % 225, (this.pos.y*this.pos.y) % 255, (this.pos.x *this.pos.y* 2) % 255);
-    //stroke(this.pos.x)
-    // textSize(this.pos.x % 20)
-    rect(this.pos.x, this.pos.y, 100, 50)
-    //ellipse(this.pos.x, this.pos.y, 100, 100)
-    pop();
-
-    
-    
-  }
+  this.hits = function(obs) {
+    if ((obs.pos.x + 25 >= this.pos.x && obs.pos.x + 25 <= (this.pos.x + 50)) && 
+      (obs.pos.y + 20 >= this.pos.y && obs.pos.y + 20 <= (this.pos.y + 40))) {
+      obs.pos.y = -400;
+      this.score++;
+    }    
+  };
+  
 }
